cc9480f920e185b5535759bf273d8fa83c7742aa
Cheney.Cheng <Cheney.Cheng@CN2211740W1.ey.net>
2017-10-25 11:42:44 +0800

    Code Commit

--- src/main/java/org/mytools/core/CharacterFormater4MD.java
+++ src/main/java/org/mytools/core/CharacterFormater4MD.java
@@ -3,9 +3,15 @@
  */
 package org.mytools.core;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.apache.commons.lang3.StringUtils;
+import org.mytools.util.FileUtils;
 
 /**
  * @author Cheney.Cheng
@@ -16,29 +22,48 @@ public class CharacterFormater4MD {
 
 	/**
 	 * @param args
+	 * @throws IOException
 	 */
-	public static void main(String[] args) {
-		String dirPath = "C:\\Work\\TT\\Books\\笔记\\开源框架\\Spring";
-		File dir = new File(dirPath);
-		if (!dir.exists()) {
-			return;
-		}
-		File[] files = FileUtils.
+	public static void main(String[] args) throws IOException {
+		String dirpath = "C:\\Work\\TT\\Books\\笔记";
+		doFormat(dirpath);
 	}
 
-	private File[] listAllFiles(File dir) {
-		if (dir == null || !dir.exists()) {
-			return null;
+	private static void doFormat(String rootpath) throws IOException {
+		File dir = new File(rootpath);
+		if (!dir.exists()) {
+			System.out.println("文件不存在");
+			return;
 		}
-		if (dir.isFile()) {
-			return new File[] { dir };
-		}
-		File[] files = null;
-		for (File file : dir.listFiles()) {
-			if (files == null) {
-				
+		File[] files = FileUtils.listAllFiles(dir, new String[] { "md", "txt" });
+		System.out.println("待格式化文件数量： " + files.length);
+		for (int i = 0; i < files.length; i++) {
+			File file = files[i];
+			String name = file.getName();
+			System.out.println("正在处理第" + (i + 1) + "个文件，文件名为： " + name);
+			String extension = name.substring(name.lastIndexOf("."));
+			String nameWithNoExtension = name.substring(0, name.lastIndexOf("."));
+			File tmp = FileUtils.createNewFile(nameWithNoExtension + "_tmp" + extension);
+			BufferedReader reader = new BufferedReader(new FileReader(file));
+			BufferedWriter writer = new BufferedWriter(new FileWriter(tmp));
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				writer.write(formatLine(line) + "\r\n");
 			}
+			reader.close();
+			writer.close();
+			file.delete();
+			tmp.renameTo(file);
 		}
+		System.out.println("文件格式化结束。");
+	}
+
+	private static String formatLine(String line) {
+		if (StringUtils.isEmpty(line)) {
+			return line;
+		}
+		return line.replaceAll("([A-Za-z0-9])([\u4e00-\u9fa5]+)", "$1 $2").replaceAll("([\u4e00-\u9fa5])([A-Za-z0-9]+)",
+				"$1 $2");
 	}
 
 }
--- src/main/java/org/mytools/util/FileUtils.java
+++ src/main/java/org/mytools/util/FileUtils.java
@@ -3,6 +3,9 @@ package org.mytools.util;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author Cheney.Cheng
@@ -12,28 +15,58 @@ import java.io.IOException;
 public class FileUtils extends org.apache.commons.io.FileUtils {
 
 	/**
-	 * 加载所有
+	 * 加载指定目录下的所有文件（包含子目录）
 	 * 
 	 * @param root
+	 * @param extensions
 	 * @return
 	 * @throws IOException
 	 */
-	public static File[] listFiles(File root) throws IOException {
+	public static File[] listAllFiles(File root, String[] extensions) throws IOException {
 		if (root == null || !root.exists()) {
 			throw new FileNotFoundException();
 		}
 		if (root.isFile()) {
 			return new File[] { root };
 		}
-		File[] files = null;
+		List<File> files = new ArrayList<>();
 		for (File file : root.listFiles()) {
-			if (files == null) {
-				files = listFiles(file);
+			if (file.isFile()) {
+				if (matchExtension(extensions, file)) {
+					files.add(file);
+				}
 			} else {
-				files = ArrayUtils.addAll(files, listFiles(file));
+				files.addAll(Arrays.asList(listAllFiles(file, extensions)));
 			}
 		}
-		return files;
+		return files.toArray(new File[files.size()]);
+	}
+
+	/**
+	 * 创建新文件
+	 * 
+	 * @param filePath
+	 * @return
+	 * @throws IOException
+	 */
+	public static File createNewFile(String filePath) throws IOException {
+		File file = new File(filePath);
+		if (file.exists()) {
+			file.delete();
+		}
+		file.createNewFile();
+		return file;
+	}
+
+	private static boolean matchExtension(String[] extensions, File file) {
+		String name = file.getName();
+		String fileExt = name.substring(name.lastIndexOf(".") + 1);
+		for (String extension : extensions) {
+			if (extension.equalsIgnoreCase(fileExt)) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
